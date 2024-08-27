cdf44e0e6588a21117c7da1d72eff6756e76e4d4
thuupx <xuanthu404@gmail.com>
2024-08-27 11:43:29 +0700

    fix: update btn and UI

--- ui/duplicate_image_finder_app.py
+++ ui/duplicate_image_finder_app.py
@@ -1,8 +1,9 @@
 import asyncio
 import threading
-import customtkinter as ctk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, StringVar
 from typing import Optional
+
+import customtkinter as ctk
 
 from find_duplicate_images.handler import find_and_move_duplicates_handler
 
@@ -11,17 +12,19 @@ class DuplicateImageFinderApp:
     def __init__(self, root: ctk.CTk):
         self.root: ctk.CTk = root
         self.loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-        self.image_dir: Optional[str] = None
+        self.image_dir: StringVar = StringVar()
 
-        self.btn_process: Optional[ctk.CTkButton] = None
+        self.btn_scan: Optional[ctk.CTkButton] = None
         self.progress_bar: Optional[ctk.CTkProgressBar] = None
 
         self.setup_ui()
         self.start_asyncio_event_loop()
 
+        self.image_dir.trace_add("write", self.on_image_dir_changed)
+
     def setup_ui(self) -> None:
         self.root.title("Find Duplicate Images")
-        self.root.geometry("768x576")
+        self.root.geometry("800x600")
 
         top_frame: ctk.CTkFrame = ctk.CTkFrame(master=self.root)
         top_frame.pack(pady=20)
@@ -30,17 +33,19 @@ class DuplicateImageFinderApp:
         bottom_frame.pack(side=ctk.BOTTOM, pady=20)
 
         btn_select_dir: ctk.CTkButton = ctk.CTkButton(
-            master=top_frame, text="Select Folder", command=self.select_dir_handler
+            master=top_frame,
+            text="Select Folder",
+            command=self.on_btn_select_dir_clicked,
         )
         btn_select_dir.pack()
 
-        self.btn_process = ctk.CTkButton(
+        self.btn_scan = ctk.CTkButton(
             master=bottom_frame,
-            text="Process",
-            command=self.on_process_click_handler,
+            text="Scan",
+            command=self.on_btn_process_clicked,
         )
-        self.btn_process.pack()
-        self.btn_process.configure(state=ctk.DISABLED)
+        self.btn_scan.pack()
+        self.btn_scan.configure(state=ctk.DISABLED)
 
         self.progress_bar = ctk.CTkProgressBar(
             master=self.root,
@@ -48,34 +53,43 @@ class DuplicateImageFinderApp:
             mode="indeterminate",
         )
 
-        self.progress_bar.pack(pady=12, padx=10)
+    def on_btn_select_dir_clicked(self) -> None:
+        directory = filedialog.askdirectory(
+            title="Select Images Folder", initialdir=self.image_dir.get()
+        )
+        if directory:
+            self.image_dir.set(directory)
 
-    def select_dir_handler(self) -> None:
-        self.image_dir = filedialog.askdirectory()
-        print("Selected dir:", self.image_dir)
-        if self.image_dir:
-            self.btn_process.configure(state=ctk.NORMAL)
+    def on_image_dir_changed(self, *args) -> None:
+        if self.image_dir.get():
+            self.btn_scan.configure(state=ctk.NORMAL)
+        else:
+            self.btn_scan.configure(state=ctk.DISABLED)
+
+    def on_btn_process_clicked(self) -> None:
+        if not self.loop.is_running():
+            self.start_asyncio_event_loop()
+        asyncio.run_coroutine_threadsafe(self.process_images(), self.loop)
 
     async def process_images(self) -> None:
-        print("Start processing", self.image_dir)
-        self.btn_process.configure(state=ctk.DISABLED)
+        self.btn_scan.configure(state=ctk.DISABLED)
+        self.progress_bar.pack(pady=12, padx=10)
         self.progress_bar.start()
 
         try:
-            results, error = await find_and_move_duplicates_handler(self.image_dir)
+            results, error = await find_and_move_duplicates_handler(
+                self.image_dir.get(), dry_run=True
+            )
             if error:
                 messagebox.showerror("Error", error)
             else:
                 print(results)
         except Exception as e:
-            print(e)
             messagebox.showerror("Error", str(e))
         finally:
             self.progress_bar.stop()
-            self.btn_process.configure(state=ctk.NORMAL)
-
-    def on_process_click_handler(self) -> None:
-        asyncio.run_coroutine_threadsafe(self.process_images(), self.loop)
+            self.progress_bar.pack_forget()
+            self.btn_scan.configure(state=ctk.NORMAL)
 
     def start_asyncio_event_loop(self) -> None:
         t = threading.Thread(
